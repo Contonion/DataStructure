92784968b19ebb1707cb157fc550d7b42e1ad6c8
Cade Jackman <engineer202@outlook.com>
2019-01-28 09:17:29 -0700

    Setup classes such as Node, Controller, and Runner.

---
+++ DataStructure/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  DataStructure
+//
+//  Created by Jackman, Cade on 1/28/19.
+//  Copyright © 2019 CadeJackman. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+using namespace std;
+class Controller
+{
+public:
+    void start();
+    
+};
+
+#endif /* Controller_hpp */
---
+++ DataStructure/Controller/Runner.cpp
@@ -0,0 +1,15 @@
+//
+//  Runner.cpp
+//  DataStructure
+//
+//  Created by Jackman, Cade on 1/28/19.
+//  Copyright © 2019 CadeJackman. All rights reserved.
+//
+
+#include <stdio.h>
+#include "Controller.hpp"
+int main()
+{
+    Controller app;
+    app.start();
+}
