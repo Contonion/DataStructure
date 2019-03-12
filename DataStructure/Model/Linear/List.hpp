b2baf0f4e93aa95afbc99a9e6cb9fafa0de0e169
Cade Jackman <engineer202@outlook.com>
2019-03-12 07:57:34 -0600

    Fixed all errors involved with FileController

--- DataStructure/Model/Linear/List.hpp
+++ DataStructure/Model/Linear/List.hpp
@@ -15,8 +15,8 @@ class List
 protected:
     int size;
 public:
-    virtual void add(Type);
-    virtual void addAtIndex(int index, Type item);
+    virtual void add(Type) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
     virtual Type remove(int index) = 0;
     virtual Type getFromIndex(int index) = 0;
     virtual int getSize() const = 0;
